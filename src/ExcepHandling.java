41041642d77042cefc50284eff918c48547f27f6
shaikfiroz1836 <shaikfiroz1836@gmail.com>
2026-01-12 13:06:37 +0530

    tst

--- src/ExcepHandling.java
+++ src/ExcepHandling.java
@@ -1,2 +1,13 @@
 public class ExcepHandling {
+    public static void main(String[] args){
+        // Try catch
+
+        int[] marks = {97,98,95};
+
+        try{System.out.println(marks[4]);}
+        catch(Exception exception){
+            System.out.println("My name is Firoz");
+
+        }
+    }
 }
---
+++ src/Method.java
@@ -0,0 +1,26 @@
+public class Method {
+    public static void PrintJava(){
+        System.out.println("Hello Java");
+    }
+    public static void PrintName(String name){
+        System.out.println(name);
+    }
+    public static void PrintSum(int a, int b){
+        System.out.println(a+b);
+    }
+    public static void main(String[] args){
+        // Methods
+//        PrintJava();
+//        PrintJava();
+//        PrintJava();
+
+//        PrintName("Firoz");
+//        PrintName("Shaik");
+//        PrintName("Hello");
+
+
+        //Functions
+        PrintSum(1,6);
+
+    }
+}
