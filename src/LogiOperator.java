14fc593fc6dbab14e1ffd400e7f4fec364895bf7
shaikfiroz1836 <shaikfiroz1836@gmail.com>
2026-01-12 11:29:21 +0530

    tst

--- src/LogiOperator.java
+++ src/LogiOperator.java
@@ -1,15 +1,50 @@
+import java.util.Scanner;
 public class LogiOperator {
     public static void main(String[] args){
         // && || operators
 
-        int a = 60;
-        int b = 40;
-        if(a<50 && b<50)
-            System.out.println("Both are less than 50");
+//        int a = 60;
+//        int b = 40;
+//        if(a<50 && b<50)
+//            System.out.println("Both are less than 50");
+//
+//        int c = 70;
+//        if (a < 50 || b < 50 || c < 50)
+//            System.out.println("atleast one is less than 50");
+//
+//        boolean IsSunUp = false;
+//        if(IsSunUp == true)
+//            System.out.println("Day");
+//        else
+//            System.out.println("Night");
 
-        int c = 70;
-        if (a < 50 || b < 50 || c < 50)
-            System.out.println("atleast one is less than 50");
+
+         /////// Wrong code
+
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("How much money do you have : ");
+//        int PenAmount = sc.nextInt();
+//        int BookAmount = sc.nextInt();
+//        if(PenAmount > 10 && BookAmount> 40)
+//            System.out.println("You can buy both");
+//        if(PenAmount >= 10 && BookAmount <= 40)
+//            System.out.println("You can buy Pen only");
+//        if(PenAmount>=10 && BookAmount >= 40)
+//            System.out.println("You can buy the book");
+//        else
+//            System.out.println("You don't have enough money");
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Amount : ");
+
+        int cash = sc.nextInt();
+
+        if(cash<10){
+            System.out.println("You don't have enough money");}
+        else if(cash>10 && cash < 40){
+            System.out.println("You can buy only pen");}
+        else{
+            System.out.println("You can buy both");}
 
     }
 }
